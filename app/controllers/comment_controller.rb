3e9867aca9192a25736b8f93440ca14daddc7031
｛Yudai Takahashi｝ <yudai.takahashi@befool.co.jp>
2018-08-16 19:24:59 +0900

    30point

--- app/controllers/comment_controller.rb
+++ app/controllers/comment_controller.rb
@@ -7,8 +7,9 @@ class CommentController < ApplicationController
     params.require(:comment).permit(:id,:title,:name,:email,:comments,:day)
   end
   def create
-    @comments = Comment.new(comment_params)
-    if @comments.save
+    @comment = Comment.new(comment_params)
+    @comments = Comment.all
+    if @comment.save
       redirect_to comments_path
     else
       render action: :comment
